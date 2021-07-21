f6c8e053cb3794ee98fda1049c30369102ac8c82
snowman07 <59627816+snowman07@users.noreply.github.com>
2021-07-21 10:14:56 -0600

    skills please cooperate

--- arrpogi/js/skills-move.js
+++ arrpogi/js/skills-move.js
@@ -1,17 +1,17 @@
 jQuery(document).ready(function () {
     /*MODIFICATION START*/
     jQuery(document).on('scroll', function () {
-        if (jQuery('html,body').scrollTop() > jQuery('main #skills-container').height()) {
+        if (jQuery(document).scrollTop() > jQuery('#skills-container').height()) {
             /*MODIFICATION END*/
             jQuery(".progress-bar").each(function () {
                 jQuery(this).find(".progress-content").animate({
                     width: jQuery(this).attr("data-percentage")
-                }, 2000);
+                }, 3000);
 
                 jQuery(this).find(".progress-number-mark").animate({
                     left: jQuery(this).attr("data-percentage")
                 }, {
-                    duration: 2000,
+                    duration: 3000,
                     step: function (now, fx) {
                         var data = Math.round(now);
                         jQuery(this).find(".percent").html(data + "%");
